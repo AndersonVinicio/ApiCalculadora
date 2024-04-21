94922cab5bcb5b7743dbcef7559c1214820b76a1
Anderson <andyvini.developer@gmail.com>
2024-04-21 20:22:03 +0200

    TEST UNITARIOS DE MULTIPLICACION

---
+++ PYTHON/operaciones/multiplicacion/multiplicacion.py
@@ -0,0 +1,6 @@
+class multiplicacion:
+    def __init__(self) -> None:
+        pass
+
+    def multiplicacion(self, num1:int, num2:int)-> int:
+        return num1 * num2
---
+++ test/multiplicacion_test.py
@@ -0,0 +1,17 @@
+import unittest
+import sys
+sys.path.insert(0, '..')
+from PYTHON.operaciones.multiplicacion.multiplicacion import multiplicacion
+
+class TestMultiplicacion(unittest.TestCase):
+    def test_sumar(self):
+        operacion = multiplicacion()
+        self.assertEqual(operacion.multiplicacion(3, 2), 6)
+        self.assertEqual(operacion.multiplicacion(2,3), 6)
+        self.assertEqual(operacion.multiplicacion(-1, -1), 1)
+        self.assertEqual(operacion.multiplicacion(2, 0), 0)
+        self.assertEqual(operacion.multiplicacion(0, 2), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
